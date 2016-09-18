ac02b48351c4c9e9a3cdadc39fa0103a3b281a69
fnets <kingoftherobots@gmail.com>
2016-09-18 23:46:20 +0000

    Added Node and Express code from https://www.raywenderlich.com/61078/write-simple-node-jsmongodb-web-service-ios-app.  Ports defined for use in cloud9.  Next up is adding Mongo.

---
+++ index.js
@@ -0,0 +1,18 @@
+var http = require('http')
+	express = require("express");
+	path = require('path');
+
+var app = express();
+app.set('port', process.env.PORT);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req,res){
+	res.send('<html><body><h1>Hello World</h1></body></html>');
+});
+app.get('/:a?/:b?/:c?', function (req,res) {
+	res.send(req.params.a + ' ' + req.params.b + ' ' + req.params.c);
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
