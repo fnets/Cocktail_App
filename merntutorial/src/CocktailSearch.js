032f3d5447816ccfd2fb333c5c9c2add48506d54
fnets <kingoftherobots@gmail.com>
2016-11-24 03:02:59 +0000

    Added CocktailSearch, but needs to properly work with the router to filter.

---
+++ merntutorial/src/CocktailSearch.js
@@ -0,0 +1,30 @@
+import SearchInput, {createFilter} from 'react-search-input'
+
+const KEYS_TO_FILTERS = ['name', 'ingredients', 'recipe', 'description']
+
+var reactSearchComponent = React.createClass({
+  getInitialState () {
+    return { searchTerm: '' }
+  },
+
+  render () {
+    const filteredCocktails = cocktails.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
+
+    return (
+      <div>
+        <SearchInput className="search-input" onChange={this.searchUpdated} />
+        {filteredCocktails.map(cocktail => {
+          return (
+                this.setState({name: newProps.initFilter.name, ingredients: newProps.initFilter.ingredients, directions: newProps.initFilter.directions});
+          )
+        })}
+      </div>
+    )
+  },
+
+  searchUpdated (term) {
+    this.setState({searchTerm: term})
+  }
+})
+
+module.exports = CocktailSearch;
